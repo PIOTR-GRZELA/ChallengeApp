b1c3c78ab81bb9c52cc5edb524ee947207f7c564
maciejdelfin <maciejdelfin@hotmail.com>
2025-01-26 06:20:09 +0100

    Dzięń 4 -26.01.2025

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello,Poland");
+﻿string Name = "Piotr";
+string Sex = "Male";
+int Age = 48;
+if (Sex == "Female" && Age > 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (Name == "Ewa" && Age == 30)
+{
+
+    Console.WriteLine("Ewa lat 30");
+}
+if (Sex == "Male" && Age<18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
